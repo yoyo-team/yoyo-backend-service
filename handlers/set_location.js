0cc5fa6906dcab4c8ff0a8e201ec1c71a6337707
孟政元 <278227739@qq.com>
2017-03-02 21:51:36 +0800

    完成set_location功能

--- handlers/set_location.js
+++ handlers/set_location.js
@@ -1,6 +1,89 @@
 let db=global.vars.db;
+let check=global.vars.check;
 
 module.exports.get=function(req,res)
 {
-
+    let query=req.query;
+    console.log(query);
+    if(!query.uid)
+    {
+        res.status(200).json
+        (
+            {
+                status:'no_uid'
+            }
+        );
+        return ;
+    }
+    if(!query.access_token)
+    {
+        res.status(200).json
+        (
+            {
+                status:'no_access_token'
+            }
+        );
+        return ;
+    }
+    if(!query.location)
+    {
+        res.status(200).json
+        (
+            {
+                status:'no_location'
+            }
+        );
+        return ;
+    }
+    check(query.uid,query.access_token)
+        .then(function()
+        {
+            let sql=`update yoyo.user set location='${query.location}' where uid='${query.uid}'`;
+            console.log(sql);
+            db.query(sql,function(err,result)
+            {
+                if(err)
+                {
+                    console.log(err);
+                    res.status(200).json
+                    (
+                        {
+                            status:'db_error'
+                        }
+                    );
+                }
+                else
+                {
+                    console.log(result);
+                    if(result.affectedRows===1)
+                    {
+                        res.status(200).json
+                        (
+                            {
+                                status:'ok'
+                            }
+                        );
+                    }
+                    else
+                    {
+                        res.status(200).json
+                        (
+                            {
+                                status:'no_effect',
+                                message:'设置location失败，用户可能不存在'
+                            }
+                        );
+                    }
+                }
+            });
+        },function()
+        {
+            res.status(200).json
+            (
+                {
+                    status:'access_denied'
+                }
+            );
+        })
+    ;
 };
