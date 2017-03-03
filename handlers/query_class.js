8040398a3b09302bdc4a5249780ca36156cee12b
孟政元 <278227739@qq.com>
2017-03-03 21:45:39 +0800

    完成query_class功能

--- handlers/query_class.js
+++ handlers/query_class.js
@@ -2,5 +2,41 @@ let db=global.vars.db;
 
 module.exports.get=function(req,res)
 {
-
+    let class_name=req.query.class_name;
+    if(!class_name)
+    {
+        res.status(200).json
+        (
+            {
+                status:'no_class_name'
+            }
+        )
+    }
+    else
+    {
+        let sql=`select * from yoyo.class where name like '%${class_name}%'`;
+        db.query(sql,function(err,rows)
+        {
+            if(err)
+            {
+                console.log(err);
+                res.status(200).json
+                (
+                    {
+                        status:'db_error'
+                    }
+                )
+            }
+            else
+            {
+                res.status(200).json
+                (
+                    {
+                        status:'ok',
+                        message:rows
+                    }
+                )
+            }
+        })
+    }
 };
