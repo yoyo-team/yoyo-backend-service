b5c980ab139e69dfb9176b6d86770ce0951c4daa
孟政元 <278227739@qq.com>
2017-03-09 12:02:40 +0800

    修改了query_class的模糊搜索范围，关键字可以是课程名，课程地点或者是发布人

--- handlers/query_class.js
+++ handlers/query_class.js
@@ -2,19 +2,19 @@ let db=global.vars.db;
 
 module.exports.get=function(req,res)
 {
-    let class_name=req.query.class_name;
-    if(!class_name)
+    let key=req.query.key;
+    if(!key)
     {
         res.status(200).json
         (
             {
-                status:'no_class_name'
+                status:'no_key'
             }
         )
     }
     else
     {
-        let sql=`select * from yoyo.class where name like '%${class_name}%'`;
+        let sql=`select * from yoyo.class where name like '%${key}%' or location like '%${key}%' or releaser like '%${key}%' ; `;
         db.query(sql,function(err,rows)
         {
             if(err)
